bb5fa65e14a64d4a340efacfcb7d45b49a667821
SeanMeguSean <fernandez.sean.marino@gmail.com>
2025-06-30 23:01:52 +0800

    sanagomana

--- api/incrementDownload.js
+++ api/incrementDownload.js
@@ -1,19 +1,19 @@
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis(process.env.UPSTASH_REDIS_REST_URL, {
-  password: process.env.UPSTASH_REDIS_REST_TOKEN,
-  tls: {},
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     await redis.incr("download-count");
     const count = await redis.get("download-count");
-    res.status(200).json({ count });
+    return res.status(200).json({ count });
   } else if (req.method === "GET") {
     const count = await redis.get("download-count");
-    res.status(200).json({ count });
+    return res.status(200).json({ count });
   } else {
-    res.status(405).end(); // Method Not Allowed
+    return res.status(405).end(); // Method Not Allowed
   }
 }
